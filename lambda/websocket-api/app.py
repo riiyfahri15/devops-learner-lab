fe775629a627a77f98fdb10f8620a1cb39c83513
riiyfahri15 <riiyfahri15@gmail.com>
2026-09-18 20:43:41 +0700

    fix: linting format

--- lambda/websocket-api/app.py
+++ lambda/websocket-api/app.py
@@ -22,7 +22,6 @@ Features:
 import base64
 import json
 import os
-import uuid
 from datetime import datetime, timezone
 
 import boto3
@@ -50,29 +49,29 @@ def get_db_connection():
 
     secret_name = os.environ.get("SECRET_NAME", "devops/rds-credentials")
     region_name = os.environ.get("AWS_REGION", "us-west-2")
-    
+
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
-    
+
     get_secret_value_response = client.get_secret_value(
         SecretId=secret_name
     )
     print(get_secret_value_response)
-    
+
     secret = get_secret_value_response['SecretString']
-    
-    secret = json.loads(secret)   
-    
+
+    secret = json.loads(secret)
+
     conn = psycopg2.connect(
-            host=secret["host"],
-            port=secret["port"],
-            dbname=secret["db_name"],
-            user=secret["db_user"],
-            password=secret["db_password"],
-            connect_timeout=10,
+        host=secret["host"],
+        port=secret["port"],
+        dbname=secret["db_name"],
+        user=secret["db_user"],
+        password=secret["db_password"],
+        connect_timeout=10,
     )
     conn.autocommit = False
     return conn
